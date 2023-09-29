0a8c7c6915b14b9879da4e5bb6c359563d0b8cc9
dalek63 <thesupremedalek63@gmail.com>
2023-09-29 20:10:02 +0200

    second commit

---
+++ src/main/java/com/pacman/pacman/PacmanApplication.java
@@ -0,0 +1,13 @@
+package com.pacman.pacman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PacmanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PacmanApplication.class, args);
+	}
+
+}

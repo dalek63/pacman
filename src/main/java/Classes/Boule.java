9484db30668fe3e13753e8b5a37eeaa2b9daccd5
BelhaddadSarah <belhaddad.sarah96@gmail.com>
2023-10-06 11:41:58 +0200

    Classes Boule & SuperBoule

---
+++ src/main/java/Classes/Boule.java
@@ -0,0 +1,17 @@
+package Classes;
+
+public class Boule {
+    private Point position;
+
+    public Boule(Point position) {
+        this.position = position;
+    }
+
+    public Point getPosition() {
+        return position;
+    }
+
+    public void setPosition(Point position) {
+        this.position = position;
+    }
+}

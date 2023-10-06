339c2b264c749abe77cd94864fa4f5f11413e3b4
BelhaddadSarah <belhaddad.sarah96@gmail.com>
2023-10-06 10:54:07 +0200

    Class PacMan

---
+++ src/main/java/Classes/PacMan.java
@@ -0,0 +1,45 @@
+package Classes;
+public abstract class PacMan extends Personnage {
+    private int vies;
+    private int vitesse;
+    private String couleur;
+
+    public PacMan(Point position, Direction direction, int vies, int vitesse, String couleur) {
+        super(position, direction);
+        this.vies = vies;
+        this.vitesse = vitesse;
+        this.couleur = couleur;
+    }
+
+    public int getVies() {
+        return vies;
+    }
+
+    public void setVies(int vies) {
+        this.vies = vies;
+    }
+
+    public int getVitesse() {
+        return vitesse;
+    }
+
+    public void setVitesse(int vitesse) {
+        this.vitesse = vitesse;
+    }
+
+    public String getCouleur() {
+        return couleur;
+    }
+
+    public void setCouleur(String couleur) {
+        this.couleur = couleur;
+    }
+
+    public void mangerPoint() {
+    }
+
+    @Override
+    public void déplacer() {
+    }
+
+}

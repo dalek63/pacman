83ea21ef2c8a7b515e5d4b39613c6c44f08efb3d
BelhaddadSarah <belhaddad.sarah96@gmail.com>
2023-10-06 10:03:28 +0200

    Class Point

---
+++ src/main/java/Classes/Point.java
@@ -0,0 +1,30 @@
+package Classes;
+
+public class Point {
+        private float positionX;
+        private float positionY;
+
+        public Point(float positionX, float positionY) {
+            this.positionX = positionX;
+            this.positionY = positionY;
+        }
+
+        // Getters & setters pour positionX et positionY
+        public float getPositionX() {
+            return positionX;
+        }
+
+        public void setPositionX(float positionX) {
+            this.positionX = positionX;
+        }
+
+        public float getPositionY() {
+            return positionY;
+        }
+
+        public void setPositionY(float positionY) {
+            this.positionY = positionY;
+        }
+}
+
+

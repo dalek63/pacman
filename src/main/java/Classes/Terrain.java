5bcc63b9f078bdb1434d61b192080c543af39de3
zackbmz <zackboum.zb@gmail.com>
2023-12-25 20:21:36 +0100

    Initialisation grille avec Murs

--- src/main/java/Classes/Terrain.java
+++ src/main/java/Classes/Terrain.java
@@ -1,14 +1,42 @@
 package Classes;
+
 public class Terrain {
-    private int[][] grille;
+    private Cellule[][] grille;
 
     public Terrain(int lignes, int colonnes) {
-        grille = new int[lignes][colonnes];
+        grille = new Cellule[lignes][colonnes];
+
+        // Initialisez chaque Cellule avec un type par défaut (par exemple, VIDE)
+        for (int i = 0; i < lignes; i++) {
+            for (int j = 0; j < colonnes; j++) {
+                grille[i][j] = new Cellule(TypeCellule.VIDE, new Point(i, j));
+            }
+        }
+    }
+
+    public void initialiserTerrainAvecMurs() {
+        // Logique pour définir les murs sur le terrain
+        grille[1][1].setType(TypeCellule.MUR);
+        grille[2][2].setType(TypeCellule.MUR);
+        // Ajoutez d'autres cellules de type mur selon vos besoins
+    }
+
+    public Cellule getCellule(int ligne, int colonne) {
+        if (estDansLimites(ligne, colonne)) {
+            return grille[ligne][colonne];
+        } else {
+            // Gérer l'erreur si les coordonnées sont hors limites
+            return null;
+        }
+    }
+
+    private boolean estDansLimites(int ligne, int colonne) {
+        return ligne >= 0 && ligne < grille.length && colonne >= 0 && colonne < grille[0].length;
     }
 
     public int obtenirValeurCellule(int ligne, int colonne) {
         if (estDansLimites(ligne, colonne)) {
-            return grille[ligne][colonne];
+            return grille[ligne][colonne].getType().ordinal();
         } else {
             // Gérer l'erreur (par exemple, renvoyer une valeur par défaut)
             return -1;
@@ -17,14 +45,12 @@ public class Terrain {
 
     public void modifierValeurCellule(int ligne, int colonne, int nouvelleValeur) {
         if (estDansLimites(ligne, colonne)) {
-            grille[ligne][colonne] = nouvelleValeur;
+            grille[ligne][colonne].setType(TypeCellule.values()[nouvelleValeur]);
         } else {
             // Gérer l'erreur (par exemple, afficher un message d'erreur)
             System.out.println("Erreur : Coordonnées hors limites.");
         }
     }
 
-    private boolean estDansLimites(int ligne, int colonne) {
-        return ligne >= 0 && ligne < grille.length && colonne >= 0 && colonne < grille[0].length;
-    }
+    // ... Ajoutez d'autres méthodes ou fonctionnalités pour la classe Terrain selon vos besoins
 }
--- src/main/java/com/pacman/pacman/PacmanApplication.java
+++ src/main/java/com/pacman/pacman/PacmanApplication.java
@@ -3,11 +3,72 @@ package com.pacman.pacman;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import Classes.Terrain;
+import Classes.TypeCellule;
+
 @SpringBootApplication
 public class PacmanApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PacmanApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PacmanApplication.class, args);
+
+        // Code de test pour le Terrain
+        testTerrain();
+    }
+
+    private static void testTerrain() {
+        int tailleGrille = 15; // Taille de la grille (15x15)
+        Terrain terrain = new Terrain(tailleGrille, 15); // Crée une grille de 15 lignes et 15 colonnes
+
+        initialiserGrille(terrain); // Initialise la grille avec les murs et les espaces vides
+
+        System.out.println("Grille avec des murs :");
+        for (int i = 0; i < tailleGrille; i++) {
+            for (int j = 0; j < 15; j++) { // Utilisation d'une taille fixe de colonnes (15)
+                int valeurCellule = terrain.obtenirValeurCellule(i, j);
+                if (valeurCellule == TypeCellule.MUR.ordinal()) {
+                    System.out.print("M "); // Affiche un mur
+                } else if (valeurCellule == TypeCellule.POINT.ordinal()) {
+                    System.out.print("P "); // Affiche le point de départ de Pac-Man
+                } else {
+                    System.out.print(". "); // Affiche un espace vide
+                }
+            }
+            System.out.println(); // Nouvelle ligne pour chaque ligne de la grille
+        }
+    }
+
+
+
+    private static void initialiserGrille(Terrain terrain) {
+        char[][] grille = {
+                {'P', '.', '.', '.', 'M', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'},
+                {'.', '.', '.', '.', 'M', '.', '.', '.', 'M', 'M', '.', '.', 'M', '.', '.'},
+                {'.', 'M', 'M', '.', 'M', 'M', '.', 'M', '.', '.', '.', '.', '.', '.', '.'},
+                {'.', '.', 'M', '.', '.', 'M', '.', 'M', '.', '.', '.', 'M', 'M', 'M', '.'},
+                {'.', '.', '.', '.', '.', '.', '.', 'M', 'M', 'M', '.', '.', '.', '.', '.'},
+                {'.', '.', 'M', '.', '.', 'M', '.', '.', 'M', '.', '.', 'M', '.', '.', '.'},
+                {'.', '.', '.', '.', '.', '.', '.', '.', '.', 'M', '.', 'M', 'M', '.', '.'},
+                {'M', 'M', 'M', '.', 'M', '.', 'M', '-', 'M', '.', 'M', '.', '.', '.', '.'},
+                {'.', '.', 'M', '.', 'M', '.', 'M', '.', 'M', '.', 'M', '.', '.', '.', '.'},
+                {'M', 'M', 'M', '.', '.', '.', 'M', 'M', 'M', '.', '.', '.', 'M', '.', '.'},
+                {'.', '.', '.', '.', '.', '.', '.', '.', 'M', '.', '.', '.', 'M', '.', '.'},
+                {'M', 'M', 'M', 'M', 'M', 'M', 'M', '.', '.', '.', '.', '.', '.', '.', '.'},
+                {'.', '.', '.', 'M', '.', '.', '.', '.', '.', 'M', '.', '.', 'M', '.', '.'},
+                {'.', '.', '.', 'M', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'},
+                {'.', '.', '.', '.', '.', 'M', 'M', 'M', '.', '.', '.', '.', '.', '.', '.'}
+        };
+
+        for (int i = 0; i < grille.length; i++) {
+            for (int j = 0; j < grille[0].length; j++) {
+                if (grille[i][j] == 'P') {
+                    terrain.modifierValeurCellule(i, j, TypeCellule.POINT.ordinal());
+                } else if (grille[i][j] == 'M') {
+                    terrain.modifierValeurCellule(i, j, TypeCellule.MUR.ordinal());
+                }
+                // Ajoutez des conditions supplémentaires si nécessaire pour d'autres caractères
+            }
+        }
+    }
 
 }

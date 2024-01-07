dd835207cf2fc23bf52975483b4726aa114394ba
zackbmz <zackboum.zb@gmail.com>
2024-01-07 19:58:31 +0100

    Nettoyage et optimisation du code

--- src/main/java/Classes/Game.java
+++ src/main/java/Classes/Game.java
@@ -43,18 +43,7 @@ public class Game {
             pacMan.deplacer(this.terrain.getGrille(), direction);
 //            this.lastDirections.add(direction);
         }
-//
-//        if (direction != null) {
-//            deplacerPacman(direction);
-//            checkCollisionPacmanFantome();
-//            checkCollisionPacmanBoule();
-//            checkGameOver();
-//        }
-//        if (!lastDirections.isEmpty()){
-//            if(direction == this.lastDirections.size() - 1){
-//                pacMan.deplacer(this.terrain.getGrille(), direction);
-//            }
-//        }
+
 
         // Mettre à jour la position des fantômes
         deplacerFantomes();
@@ -67,15 +56,7 @@ public class Game {
         afficherGrille(terrain.getGrille());
         this.started = true;
         initialiserJeu();
-//        updateGrid(1);
-//        updateGrid(1);
-//        updateGrid(1);
-//        updateGrid(1);
-//        updateGrid(2);
-//        for (int i = 0; i< x; i++){
-//            System.out.println("itération : "+i);
-//
-//        }
+
     }
 
     public void finJeu(){
